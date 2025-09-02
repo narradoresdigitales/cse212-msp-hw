94b1e756288a96899d1754288e2a7edd01c49c61
sil <narradoresdigitales@gmail.com>
2025-09-02 04:59:22 -0500

    wk1 teach activity complete

--- week01/teach/Divisors.cs
+++ week01/teach/Divisors.cs
@@ -18,7 +18,17 @@ public static class Divisors {
     /// <returns>List of divisors</returns>
     private static List<int> FindDivisors(int number) {
         List<int> results = new();
-        // TODO problem 1
+
+        results.Add(1);
+
+        for (int i = 2; i < number; i++)
+        {
+            if (number % i == 0)
+            {
+                results.Add(i);
+            }
+
+        }
         return results;
     }
 }
